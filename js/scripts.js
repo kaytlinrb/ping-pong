06601928d0ed09bd3a78f183a927c160adef2ec9
Kaytlin Borel <kaytlinrb@gmail.com>
2017-03-31 14:23:17 -0700

    adds ping-pong when number is divisible by 15

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,14 @@
 var pingPonger = function (number) {
   var name = "";
   for (var index=0; index<1; index++){
-  if (number %3 === 0 ) {
+  if ((number %3 === 0) && (number %15 !== 0)) {
     name ="ping"
   }
-  else if (number %5 === 0 ) {
+  else if ((number %5 === 0 ) && (number %15 !==0)) {
     name = "pong"
+  }
+  else if (number %15 === 0) {
+    name="ping-pong"
   }
   return name;
 }
