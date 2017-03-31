e3bc0fd0c877d9b428350838ca56a3289f5e2756
Kaytlin Borel <kaytlinrb@gmail.com>
2017-03-31 13:57:38 -0700

    restore code to show user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,12 @@
 
-// var operations = [userInput%3]
-// var resultArray = []
-//
-// var pingPongMachine = function(number) {
-//
-//   for (var index=0; index < operations.length; index ++){
-//     if (operations.splice(0) === 0){
-//       resultArray.push("Ping")
-//       console.log(resultArray)
-//     };
-//   };
-// };
-var result = 0
-var pingPongMachine = function(number) {
-for (var index=0; index<2; index++)
-  if (userInput % 3 === 0) {
-    result = "ping";
-    return result;
 
-  }
-}
 
-$(function(){
-  $("#pingPongForm").submit(function(event){
+
+
+$(function() {
+  $("form").submit(function(event){
     event.preventDefault();
-    var userInput = parseInt($("#numberInput").val())
-
-    if (userInput ===0){
-      alert("please enter a number other than zero")
-    }
-
-    // var result = pingPongMachine(userInput);
-    $("#returnSection").append("<li>" + result + "</li>")
-  });
-});
+    var userInput= parseInt($("#numberInput").val())
+  $("#returnSection").append("<li>" + userInput + "</li>")
+  })
+})
