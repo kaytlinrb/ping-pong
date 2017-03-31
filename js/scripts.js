9887fc7a07c1939eb2663154cac1be0ed917a1ff
Kaytlin Borel <kaytlinrb@gmail.com>
2017-03-31 12:00:55 -0700

    create for loop to apply operations to user input to determine divisors. not currently functional

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,16 @@
-//
-// var pingPongMachine = function(number) {
-//   var input = userInput
-// }
+var userInput = parseInt($("#numberInput").val())
+var operations = [userInput%3]
+var resultArray = []
+
+var pingPongMachine = function(number) {
+
+  for (var index=0; index < operations.length; index ++){
+    if (operations.splice(0) === 0){
+      resultArray.push("Ping")
+      console.log(resultArray)
+    };
+  };
+};
 
 
 
@@ -10,9 +19,12 @@
 $(function(){
   $("form").submit(function(event){
     event.preventDefault();
-    var userInput = parseInt($("#numberInput").val())
-    // var result = pingPongMachine(userInput);
-    $("#returnSection").append("<li>" + userInput + "</li>")
+
+    if("#numberInput"===0){
+      alert("please enter a number other than zero")
+    }
+    var result = pingPongMachine(userInput);
+    $("#returnSection").append("<li>" + resultArray + "</li>")
   });
 
 });
