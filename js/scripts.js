b62727ae9bc2ba6ef7630dd649dd2f8dd00e4182
Kaytlin Borel <kaytlinrb@gmail.com>
2017-03-31 14:15:47 -0700

    added spec to return pong when number is divisible by 5

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,23 @@
 
-
-
+var pingPonger = function (number) {
+  var name = "";
+  for (var index=0; index<1; index++){
+  if (number %3 === 0 ) {
+    name ="ping"
+  }
+  else if (number %5 === 0 ) {
+    name = "pong"
+  }
+  return name;
+}
+}
 
 
 $(function() {
-  $("form").submit(function(event){
-    event.preventDefault();
-    var userInput= parseInt($("#numberInput").val())
-  $("#returnSection").append("<li>" + userInput + "</li>")
-  })
-})
+    $("form").submit(function(event){
+      event.preventDefault();
+      var userInput= parseInt($("#numberInput").val())
+      var result = pingPonger(userInput);
+    $("#returnSection").append("<li>" + result + "</li>")
+  });
+});
