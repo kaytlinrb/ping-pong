2c08e2b956299a4bd198075539fb8859e201432f
Kaytlin Borel <kaytlinrb@gmail.com>
2017-03-31 13:23:05 -0700

    attempted simpler formula to use for loop. still non functional

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,36 @@
-var userInput = parseInt($("#numberInput").val())
-var operations = [userInput%3]
-var resultArray = []
 
+// var operations = [userInput%3]
+// var resultArray = []
+//
+// var pingPongMachine = function(number) {
+//
+//   for (var index=0; index < operations.length; index ++){
+//     if (operations.splice(0) === 0){
+//       resultArray.push("Ping")
+//       console.log(resultArray)
+//     };
+//   };
+// };
+var result = 0
 var pingPongMachine = function(number) {
+for (var index=0; index<2; index++)
+  if (userInput % 3 === 0) {
+    result = "ping";
+    return result;
 
-  for (var index=0; index < operations.length; index ++){
-    if (operations.splice(0) === 0){
-      resultArray.push("Ping")
-      console.log(resultArray)
-    };
-  };
-};
-
-
-
-
+  }
+}
 
 $(function(){
-  $("form").submit(function(event){
+  $("#pingPongForm").submit(function(event){
     event.preventDefault();
+    var userInput = parseInt($("#numberInput").val())
 
-    if("#numberInput"===0){
+    if (userInput ===0){
       alert("please enter a number other than zero")
     }
-    var result = pingPongMachine(userInput);
-    $("#returnSection").append("<li>" + resultArray + "</li>")
-  });
 
+    // var result = pingPongMachine(userInput);
+    $("#returnSection").append("<li>" + result + "</li>")
+  });
 });
